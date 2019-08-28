95fd5ded027986e4b94a34030853905e097b7fee
huayj <huayjh@yonyou.com>
2019-08-28 11:13:31 +0800

    修改bug

--- build/Table.js
+++ build/Table.js
@@ -848,25 +848,23 @@ var Table = function (_Component) {
     var lazyEndIndex = props.lazyLoad && props.lazyLoad.endIndex ? props.lazyLoad.endIndex : -1;
     for (var i = 0; i < data.length; i++) {
       var isHiddenExpandIcon = void 0;
-      if (props.showRowNum) {
-        switch (props.showRowNum.type) {
-          case 'number':
-            {
-              data[i][props.showRowNum.key || '_index'] = (props.showRowNum.base || 0) + i;
-              break;
-            }
-          case 'ascii':
-            {
-              data[i][props.showRowNum.key || '_index'] = String.fromCharCode(i + (props.showRowNum.base || '0').charCodeAt());
-              break;
-            }
-          default:
-            {
-              data[i][props.showRowNum.key || '_index'] = (props.showRowNum.base || 0) + i;
-              break;
-            }
-        }
-      }
+      // if ( props.showRowNum ){
+      //   switch(props.showRowNum.type){
+      //     case 'number':{
+      //       data[i][props.showRowNum.key || '_index'] = (props.showRowNum.base || 0) + i;
+      //       break;
+      //     }
+      //     case 'ascii': {
+      //       data[i][props.showRowNum.key || '_index'] = String.fromCharCode(i + (props.showRowNum.base || '0').charCodeAt());
+      //       break;
+      //     }
+      //     default: {
+      //       data[i][props.showRowNum.key || '_index'] = (props.showRowNum.base || 0) + i;
+      //       break;
+      //     }
+      //   }
+
+      // } 
       var record = data[i];
       var key = this.getRowKey(record, i);
       var childrenColumn = record[childrenColumnName];
